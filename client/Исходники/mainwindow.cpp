e3f93dc49cc7ff89fadac600b98c5fda380c8a1c
AnyaGl <51699328+AnyaGl@users.noreply.github.com>
2020-01-21 17:00:54 +0300

    Add files via upload

--- client/Исходники/mainwindow.cpp
+++ client/Исходники/mainwindow.cpp
@@ -1,12 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setWindowTitle("CreativeMessenger");
 
     socket = new QTcpSocket(this);
         connect(socket,SIGNAL(readyRead()),this,SLOT(sockReady()));
@@ -27,36 +27,39 @@ void MainWindow::sockDisc()
 
 void MainWindow::addContact()
 {
+    qDebug() << text;
     if (doc.object().value("Param").toString() == "yes")
-    {
-        bool check = true;
-        for (int i = 0; i < ui->listContact->count(); i++)
-        {
-            if (ui->listContact->item(i)->text() == text)
-                check = false;
-        }
-        if (check)
-            ui->listContact->addItem(text);
-        else
-            QMessageBox::warning(this, "Внимание","Пользователь уже добавлен");
-        qDebug() << "добавили text: " << text;
-    } else {
+        ui->listContact->addItem(text);
+    else
         QMessageBox::warning(this, "Внимание","Пользователя не существует. Проверьте введенные данные");
-    }
+
 }
 
 void MainWindow::connectUser()
 {
     if (doc.object().value("Param").toString() == "yes")
     {
-        user0 = text;
-        QMessageBox::information(this, "Добро пожаловать!","Вы успешно авторизовались");
+        user0 = text.split("/")[0];
+        //QMessageBox::information(this, "Добро пожаловать!","Добро пожаловать!");
+
         ui->logIn->setDisabled(true);
+        ui->signUp->setDisabled(true);
         ui->currLogin->clear();
         ui->currLogin->setReadOnly(true);
         ui->currLogin->setPlaceholderText("");
+        ui->currPass->clear();
+        ui->currPass->setReadOnly(true);
+        ui->currPass->setPlaceholderText("");
+
+        int i = 0;
+        while (!doc.object().value("List")[i].isUndefined())
+        {
+            ui->listContact->addItem(doc.object().value("List")[i].toString());
+            i += 1;
+        }
+
     } else {
-        QMessageBox::warning(this, "Внимание","Логин занят");
+        QMessageBox::warning(this, "Внимание","Логин или пароль неверный");
     }
 }
 
@@ -72,6 +75,48 @@ void MainWindow::printMessage()
     }
 }
 
+void MainWindow::showMessages()
+{
+    if (doc.object().value("Param").toString() == "yes")
+    {
+        int i = 0;
+        while (!doc.object().value("List")[i].isUndefined())
+        {
+            ui->msgField->append(doc.object().value("List")[i].toString());
+            i += 1;
+        }
+    }
+}
+
+
+void MainWindow::signUpUser()
+{
+    if (doc.object().value("Param").toString() == "yes")
+    {
+        QMessageBox::information(this, "Добро пожаловать!","Вы успешно зарегистрировались");
+        ui->currPass->clear();
+        ui->currLogin->clear();
+    }
+    else
+        QMessageBox::warning(this, "Внимание","Логин занят");
+}
+
+void MainWindow::delContact()
+{
+    if (doc.object().value("Param").toString() == "ok")
+    {
+        if (user1 == ui->listContact->item(ui->listContact->currentRow())->text())
+        {
+            ui->msgField->clear();
+            ui->msgField->setPlaceholderText("Выберите пользователя");
+            ui->sendButton->setDisabled(true);
+        }
+
+        QListWidgetItem *it = ui->listContact->item(ui->listContact->currentRow());
+        delete it;
+    }
+}
+
 void MainWindow::sockReady()
 {
     if (socket->waitForConnected(500))
@@ -83,12 +128,18 @@ void MainWindow::sockReady()
          qDebug() << "получили ответ: " << doc.object().value("Param").toString();
          if (docError.errorString().toInt() == QJsonParseError::NoError)
          {
-             if (doc.object().value("Method").toString() == "GET" && doc.object().value("Object").toString() == "contact")
-                 addContact();
              if (doc.object().value("Method").toString() == "POST" && doc.object().value("Object").toString() == "contact")
-                 connectUser();
+                 addContact();
              if (doc.object().value("Method").toString() == "POST" && doc.object().value("Object").toString() == "message")
                  printMessage();
+             if (doc.object().value("Method").toString() == "GET" && doc.object().value("Object").toString() == "user")
+                 connectUser();
+             if (doc.object().value("Method").toString() == "GET" && doc.object().value("Object").toString() == "messages")
+                 showMessages();
+             if (doc.object().value("Method").toString() == "POST" && doc.object().value("Object").toString() == "user")
+                 signUpUser();
+             if (doc.object().value("Method").toString() == "DELETE" && doc.object().value("Object").toString() == "contact")
+                 delContact();
          }
     }
 
@@ -96,7 +147,7 @@ void MainWindow::sockReady()
 
 void MainWindow::on_sendButton_clicked()
 {
-    text = ui->currMsg->text();
+    text = user0 + ": " + ui->currMsg->text();
     std::string myString = "{\"Method\":\"POST\",\"Object\":\"message\",\"Param\":\"\", \"Msg\":{\"UserFrom\":\"" + user0.toStdString() + "\",\"UserTo\":\"" + user1.toStdString() + "\",\"Text\":\"" + text.toStdString() + "\"}}";
 
     if (socket->isOpen() && text.length() > 0)
@@ -120,39 +171,52 @@ void MainWindow::on_addButton_clicked()
                                  QLineEdit::Normal,
                                  QDir::home().dirName(), &ok);
     qDebug() << "получили text: " << text;
-
     // проверяем существование пользователя
-    std::string myString = "{\"Method\":\"GET\",\"Object\":\"contact\",\"Param\":\"" + text.toStdString() + "\"}";
-
-    if (socket->isOpen())
+    bool check = true;
+    for (int i = 0; i < ui->listContact->count(); i++)
     {
-
-        //socket->waitForBytesWritten(500);
-        socket->write(myString.c_str());
-        qDebug() << "отправили запрос: " << myString.c_str();
-
+        if (ui->listContact->item(i)->text() == text)
+            check = false;
     }
+    if (check)
+    {
+        std::string myString = "{\"Method\":\"POST\",\"Object\":\"contact\",\"Param\":\"" + (user0 + "/" + text).toStdString() + "\"}";
+
+        if (socket->isOpen() && text.length() > 0)
+        {
+            socket->write(myString.c_str());
+            qDebug() << "отправили запрос: " << myString.c_str();
+        }
+    }
+    else
+        QMessageBox::warning(this, "Внимание","Пользователь уже добавлен");
+
 }
 
 void MainWindow::on_delButton_clicked()
 {
-    QListWidgetItem *it = ui->listContact->item(ui->listContact->currentRow());
-    delete it;
+    text = user0 + "/" + ui->listContact->item(ui->listContact->currentRow())->text();
+    std::string myString = "{\"Method\":\"DELETE\",\"Object\":\"contact\",\"Param\":\"" + text.toStdString() + "\"}";
+
+    if (socket->isOpen())
+    {
+        socket->write(myString.c_str());
+        qDebug() << "отправили запрос: " << myString.c_str();
+    }
 }
 
 void MainWindow::on_logIn_clicked()
 {
-    text = ui->currLogin->text();
+    QString login = ui->currLogin->text();
+    QString password = ui->currPass->text();
+    text =  login + "/" + password;
 
-    std::string myString = "{\"Method\":\"POST\",\"Object\":\"contact\",\"Param\":\"" + text.toStdString() + "\"}";
+    std::string myString = "{\"Method\":\"GET\",\"Object\":\"user\",\"Param\":\"" + text.toStdString() + "\"}";
 
-    if (socket->isOpen() && text.length() > 0)
+    if (socket->isOpen() && login.length() > 0 && password.length() > 0)
     {
-
-        //socket->waitForBytesWritten(500);
         socket->write(myString.c_str());
         qDebug() << "отправили запрос: " << myString.c_str();
-
     }
 }
 
@@ -161,6 +225,31 @@ void MainWindow::on_listContact_doubleClicked(const QModelIndex &index)
     user1 = ui->listContact->item(index.row())->text();
     ui->msgField->clear();
     ui->msgField->setPlaceholderText("Вы можете начинать беседу с " + user1);
-    //ui->sendButton->setDisabled(false);
+
+    std::string myString = "{\"Method\":\"GET\",\"Object\":\"messages\",\"Param\":\"" + (user0 + "/" + user1).toStdString() + "\"}";
+
+    if (socket->isOpen())
+    {
+        socket->write(myString.c_str());
+        qDebug() << "отправили запрос: " << myString.c_str();
+    }
+    ui->sendButton->setDisabled(false);
     //ui->currMsg->setReadOnly(false);
+}
+
+void MainWindow::on_signUp_clicked()
+{
+    QString login = ui->currLogin->text();
+    QString password = ui->currPass->text();
+    if (login.contains("/") || password.contains("/"))
+        QMessageBox::warning(this, "Внимание","Символ '/' недопустим");
+    text =  login + "/" + password;
+
+    std::string myString = "{\"Method\":\"POST\",\"Object\":\"user\",\"Param\":\"" + text.toStdString() + "\"}";
+
+    if (socket->isOpen() && login.length() > 0 && password.length() > 0)
+    {
+        socket->write(myString.c_str());
+        qDebug() << "отправили запрос: " << myString.c_str();
+    }
 }
